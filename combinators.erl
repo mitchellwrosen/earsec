34cd19ccea1941ef873bc4946a36fe5bb7f5b27a
Mitchell Rosen <mitchellwrosen@gmail.com>
2014-12-14 21:06:12 -0800

    Added uncurried versions of core funcs, plus new combinators

--- combinators.erl
+++ combinators.erl
@@ -1,12 +1,22 @@
 -module(combinators).
 
 -export([between/1,
+         between/2,
+         between/3,
          choice/1,
          count/1,
+         count/2,
+         end_by0/1,
+         end_by0/2,
+         end_by1/1,
+         end_by1/2,
          many0/1,
          many1/1,
          option/1,
+         option/2,
          optional/1,
+         sep_by0/1,
+         sep_by1/1,
          sequence/1
         ]).
 
@@ -15,78 +25,141 @@
 % Apply a parser zero or more times.
 -spec many0(parser()) -> parser().
 many0(Parser) ->
-    Lhs = many1(Parser),
-    Rhs = core:pure([]),
-    (core:alt(Lhs))(Rhs).
+    core:alt(many1(Parser), core:pure([])).
 
 % Apply a parser one or more times.
 -spec many1(parser()) -> parser().
 many1(Parser) ->
-    Cons = fun(R) ->
-               fun(Rs) ->
-                   [R|Rs]
-               end
-           end,
-    ((core:lift2(Cons))(Parser))(many0(Parser)).
+    core:lift2(cons(), Parser, many0(Parser)).
 
 % Sequence a (non-empty) list of parsers.
 -spec sequence([parser()]) -> parser().
 sequence([P]) ->
-    F = fun(R) -> [R] end,
-    (core:lift(F))(P);
+    core:lift(fun(R) -> [R] end, P);
 sequence([P|Ps]) ->
-    F = fun(R) ->
-            fun(Rs) ->
-                [R|Rs]
-            end
-        end,
-    ((core:lift2(F))(P))(sequence(Ps)).
+    core:lift2(cons(), P, sequence(Ps)).
 
 % Apply parsers in order until one succeeds.
 -spec choice([parser()]) -> parser().
 choice(Ps) ->
-    Step = fun(A, B) -> (core:alt(A))(B) end,
-    lists:foldr(Step, core:empty(), Ps).
+    lists:foldr(fun core:alt/2, core:empty(), Ps).
 
 % Apply a parser N times.
 -spec count(integer()) -> fun((parser()) -> parser()).
 count(N) ->
     fun(Parser) ->
-        sequence(replicate(N, Parser))
+        count(N, Parser)
     end.
+
+-spec count(integer(), parser()) -> parser().
+count(N, Parser) ->
+    sequence(replicate(N, Parser)).
 
 % Run a parser between two others, returning the value parsed by the middle one.
 -spec between(parser()) -> fun((parser()) -> fun((parser()) -> parser())).
 between(Open) ->
     fun(Between) ->
-        fun(Close) ->
-            (core:then(Open))(
-                (core:bind(Between))(
-                    fun(Result) ->
-                        (core:then(Close))(
-                            core:pure(Result)
-                        )
-                    end
-                )
-            )
-        end
+        between(Open, Between)
     end.
+
+-spec between(parser(), parser()) -> fun((parser()) -> parser()).
+between(Open, Between) ->
+    fun(Close) ->
+        between(Open, Between, Close)
+    end.
+
+-spec between(parser(), parser(), parser()) -> parser().
+between(Open, Between, Close) ->
+    core:then(Open,
+        core:bind(Between,
+            fun(Result) ->
+                core:then(Close, core:pure(Result))
+            end
+        )
+    ).
 
 % Applies a parser; if it fails, succeed with the given value.
 -spec option(term()) -> fun((parser()) -> parser()).
 option(Default) ->
     fun(Parser) ->
-        (core:alt(Parser))(core:pure(Default))
+        option(Default, Parser)
     end.
+
+-spec option(term(), parser()) -> parser().
+option(Default, Parser) ->
+    core:alt(Parser, core:pure(Default)).
 
 % Apply a parser. Whether it succeeds or fails, succeed with 'ok'.
 -spec optional(parser()) -> parser().
 optional(Parser) ->
-    Lhs = (core:then(Parser))(core:pure(ok)),
-    Rhs = core:pure(ok),
-    (core:alt(Lhs))(Rhs).
+    PureOk = core:pure(ok),
+    core:alt(core:then(Parser, PureOk), PureOk).
+
+% Parse zero or more times, each separated by another parser.
+-spec sep_by0(parser()) -> fun((parser()) -> parser()).
+sep_by0(Parser) ->
+    fun(Sep) ->
+        sep_by0(Parser, Sep)
+    end.
+
+-spec sep_by0(parser(), parser()) -> parser().
+sep_by0(Parser, Sep) ->
+    core:alt(sep_by1(Parser, Sep), core:pure([])).
+
+% Parse one or more times, each separated by another parser.
+-spec sep_by1(parser()) -> fun((parser()) -> parser()).
+sep_by1(Parser) ->
+    fun(Sep) ->
+        sep_by1(Parser, Sep)
+    end.
+
+-spec sep_by1(parser(), parser()) -> parser().
+sep_by1(Parser, Sep) ->
+    core:bind(Parser,
+        fun(X) ->
+            XsParser = many0(core:then(Sep, Parser)),
+            core:bind(XsParser,
+                fun(Xs) ->
+                    core:pure([X|Xs])
+                end
+            )
+        end
+    ).
+
+-spec end_by0(parser()) -> fun((parser()) -> parser()).
+end_by0(Parser) ->
+    fun(Sep) ->
+        end_by0(Parser, Sep)
+    end.
+
+-spec end_by0(parser(), parser()) -> parser().
+end_by0(Parser, Sep) ->
+    many0(endby_body(Parser, Sep)).
+
+-spec end_by1(parser()) -> fun((parser()) -> parser()).
+end_by1(Parser) ->
+    fun(Sep) ->
+        end_by1(Parser, Sep)
+    end.
+
+-spec end_by1(parser(), parser()) -> parser().
+end_by1(Parser, Sep) ->
+    many1(endby_body(Parser, Sep)).
+
+endby_body(Parser, Sep) ->
+    core:bind(Parser,
+        fun(Ret) ->
+            core:then(Sep, core:pure(Ret))
+        end).
 
 %% -----------------------------------------------------------------------------
+
+cons() ->
+    fun(X) ->
+        fun(Xs) ->
+            [X|Xs]
+        end
+    end.
 
 -spec replicate(integer(), term()) -> [term()].
 replicate(N, Term) -> [Term || _ <- lists:seq(1, N)].
--- core.erl
+++ core.erl
@@ -1,14 +1,20 @@
 -module(core).
 
 -export([alt/1,
+         alt/2,
          app/1,
+         app/2,
          bind/1,
+         bind/2,
          empty/0,
          join/1,
          lift/1,
+         lift/2,
          lift2/1,
+         lift2/3,
          pure/1,
-         then/1
+         then/1,
+         then/2
         ]).
 
 -include("earsec.hrl").
@@ -17,26 +23,37 @@
 -spec lift(fun((term()) -> term())) -> fun((parser()) -> parser()).
 lift(F) ->
     fun(ParserA) ->
-        fun(State) ->
-            case ParserA(State) of
-                {{ok, _Position, _Remainder} = State, A} ->
-                    {State, F(A)};
-                Error ->
-                    Error
-            end
+        lift(F, ParserA)
+    end.
+
+-spec lift(fun((term()) -> term()), parser()) -> parser().
+lift(F, ParserA) ->
+    fun(State) ->
+        case ParserA(State) of
+            {{ok, _Position, _Remainder} = State, A} ->
+                {State, F(A)};
+            Error ->
+                Error
         end
     end.
+
 
 % Map a 2-ary function over a parser.
 -spec lift2(fun((term()) -> fun((term()) -> term()))) -> fun((parser()) -> fun((parser()) -> parser())).
 lift2(F) ->
     fun(ParserA) ->
-        fun(ParserB) ->
-            ParserF  = pure(F),
-            ParserBC = (app(ParserF))(ParserA),
-            (app(ParserBC))(ParserB)
-        end
+        lift2(F, ParserA)
     end.
+
+-spec lift2(fun((term()) -> fun((term()) -> term())), parser()) -> fun((parser()) -> parser()).
+lift2(F, ParserA) ->
+    fun(ParserB) ->
+        lift2(F, ParserA, ParserB)
+    end.
+
+-spec lift2(fun((term()) -> fun((term()) -> term())), parser(), parser()) -> parser().
+lift2(F, ParserA, ParserB) ->
+    app(app(pure(F), ParserA), ParserB).
 
 % Lift a term into an trivial, accepting parser.
 -spec pure(term()) -> parser().
@@ -49,12 +66,16 @@ pure(Term) ->
 -spec app(parser()) -> fun((parser()) -> parser()).
 app(ParserF) ->
     fun(ParserA) ->
-        (bind(ParserF))(fun(F) ->
-            (bind(ParserA))(fun(A) ->
-                pure(F(A))
-            end)
-        end)
+        app(ParserF, ParserA)
     end.
+
+-spec app(parser(), parser()) -> parser().
+app(ParserF, ParserA) ->
+    bind(ParserF, fun(F) ->
+        bind(ParserA, fun(A) ->
+            pure(F(A))
+        end)
+    end).
 
 % Parser that always fails.
 -spec empty() -> parser().
@@ -67,13 +88,17 @@ empty() ->
 -spec alt(parser()) -> fun((parser()) -> parser()).
 alt(ParserA) ->
     fun(ParserB) ->
-        fun(State) ->
-            case ParserA(State) of
-                {{ok, _Position, _Remainder}, _Result} = Result ->
-                    Result;
-                _ ->
-                    ParserB(State)
-            end
+        alt(ParserA, ParserB)
+    end.
+
+-spec alt(parser(), parser()) -> parser().
+alt(ParserA, ParserB) ->
+    fun(State) ->
+        case ParserA(State) of
+            {{ok, _Position, _Remainder}, _Result} = Result ->
+                Result;
+            _ ->
+                ParserB(State)
         end
     end.
 
@@ -81,13 +106,17 @@ alt(ParserA) ->
 -spec bind(parser()) -> fun((fun((term()) -> parser())) -> parser()).
 bind(ParserA) ->
     fun(F) ->
-        fun(State1) ->
-            case ParserA(State1) of
-                {{ok, _Position, _Remainder} = State2, A} ->
-                    (F(A))(State2);
-                Error ->
-                    Error
-            end
+        bind(ParserA, F).
+    end.
+
+-spec bind(parser(), fun((term()) -> parser())) -> parser().
+bind(ParserA, F) ->
+    fun(State1) ->
+        case ParserA(State1) of
+            {{ok, _Position, _Remainder} = State2, A} ->
+                (F(A))(State2);
+            Error ->
+                Error
         end
     end.
 
@@ -95,11 +124,14 @@ bind(ParserA) ->
 -spec then(parser()) -> fun((parser()) -> parser()).
 then(ParserA) ->
     fun(ParserB) ->
-        (bind(ParserA))(fun(_) -> ParserB end)
+        then(ParserA, ParserB).
     end.
+
+-spec then(parser(), parser()) -> parser().
+then(ParserA, ParserB) ->
+    bind(ParserA, fun(_) -> ParserB end).
 
 % Collapse a parser of parsers into a parser.
 -spec join(parser()) -> parser().
 join(ParserPA) ->
-    (bind(ParserPA))(fun(ParserA) -> ParserA end).
-
+    bind(ParserPA, fun(ParserA) -> ParserA end).
