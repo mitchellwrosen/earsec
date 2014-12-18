3b0387cb2f2c5008d3f21f9088453956edfa30ff
mrosen <mrosen@machinezone.com>
2014-12-18 23:45:51 +0000

    Parameterize parser type

--- include/earsec.hrl
+++
@@ -1,3 +0,0 @@
--type parser() :: fun((binary()) -> {ok, {term(), integer(), binary()}}
-                                  | {error, {term(), integer(), binary()}}).
-
--- src/earsec.erl
+++ src/earsec.erl
@@ -30,10 +30,13 @@
          uint64/0
         ]).
 
--include("earsec.hrl").
+-export_type([parser/1]).
+
+-type parser(Term) :: fun((binary()) -> {ok, {Term, integer(), binary()}}
+                                      | {error, {Term, integer(), binary()}}).
 
 % Run a parser over input.
--spec parse(parser(), binary()) -> {ok, term()} | {error, {term(), integer(), binary()}}.
+-spec parse(parser(A :: term()), binary()) -> {ok, A} | {error, {A, integer(), binary()}}.
 parse(Parser, Input) ->
     case Parser(Input) of
         {ok, {Result, _, <<>>}} ->
@@ -49,28 +52,28 @@ parse(Parser, Input) ->
 % ------------------------------------------------------------------------------
 
 % Map a function over a parser.
--spec lift(fun((term()) -> term()), parser()) -> parser().
+-spec lift(fun((A) -> B), parser(A)) -> parser(B).
 lift(F, ParserA) ->
     app(pure(F), ParserA).
 
 % Map a 2-ary function over a parser.
--spec lift2(fun((term(), term()) -> term()), parser(), parser()) -> parser().
+-spec lift2(fun((A, B) -> C), parser(A), parser(B)) -> parser(C).
 lift2(F, ParserA, ParserB) ->
     app(app(pure(uncurry(F)), ParserA), ParserB).
 
--spec lift3(fun((term(), term(), term()) -> term()), parser(), parser(), parser()) -> parser().
+-spec lift3(fun((A, B, C) -> D), parser(A), parser(B), parser(C)) -> parser(D).
 lift3(F, ParserA, ParserB, ParserC) ->
     app(app(app(pure(uncurry2(F)), ParserA), ParserB), ParserC).
 
 % Lift a term into an trivial, accepting parser.
--spec pure(term()) -> parser().
+-spec pure(A) -> parser(A).
 pure(Term) ->
     fun(Input) ->
         {ok, {Term, 0, Input}}
     end.
 
 % Apply a function parser over a parser.
--spec app(parser(), parser()) -> parser().
+-spec app(parser(fun((A) -> B)), parser(A)) -> parser(B).
 app(ParserF, ParserA) ->
     bind(ParserF, fun(F) ->
         bind(ParserA, fun(A) ->
@@ -79,14 +82,14 @@ app(ParserF, ParserA) ->
     end).
 
 % Parser that always fails.
--spec empty() -> parser().
+-spec empty() -> parser(A) when A :: term().
 empty() ->
     fun(Input) ->
         {error, {"empty", 0, Input}}
     end.
 
 % Try one parser, and if it fails, try another.
--spec alt(parser(), parser()) -> parser().
+-spec alt(parser(A :: term()), parser(A)) -> parser(A).
 alt(ParserA, ParserB) ->
     fun(Input) ->
         case ParserA(Input) of
@@ -98,7 +101,7 @@ alt(ParserA, ParserB) ->
     end.
 
 % Bind the result of one parser to a parser function.
--spec bind(parser(), fun((term()) -> parser())) -> parser().
+-spec bind(parser(A), fun((A) -> parser(B))) -> parser(B).
 bind(ParserA, F) ->
     fun(Input1) ->
         case ParserA(Input1) of
@@ -113,12 +116,12 @@ bind(ParserA, F) ->
     end.
 
 % Run one parser, then toss the result and run a second.
--spec then(parser(), parser()) -> parser().
+-spec then(parser(A), parser(B)) -> parser(B) when A :: term().
 then(ParserA, ParserB) ->
     bind(ParserA, fun(_) -> ParserB end).
 
 % Collapse a parser of parsers into a parser.
--spec join(parser()) -> parser().
+-spec join(parser(Parser)) -> Parser when Parser :: parser(A), A :: term().
 join(ParserPA) ->
     bind(ParserPA, fun(ParserA) -> ParserA end).
 
@@ -127,34 +130,34 @@ join(ParserPA) ->
 % ------------------------------------------------------------------------------
 
 % Apply a parser zero or more times.
--spec many0(parser()) -> parser().
+-spec many0(parser(A)) -> parser([A]).
 many0(Parser) ->
     alt(many1(Parser), pure([])).
 
 % Apply a parser one or more times.
--spec many1(parser()) -> parser().
+-spec many1(parser(A)) -> parser([A]).
 many1(Parser) ->
     lift2(fun cons/2, Parser, many0(Parser)).
 
 % Sequence a (non-empty) list of parsers.
--spec sequence([parser()]) -> parser().
+-spec sequence([parser(A)]) -> parser([A]).
 sequence([P]) ->
     lift(fun(R) -> [R] end, P);
 sequence([P|Ps]) ->
     lift2(fun cons/2, P, sequence(Ps)).
 
 % Apply parsers in order until one succeeds.
--spec choice([parser()]) -> parser().
+-spec choice([parser(A)]) -> parser(A).
 choice(Ps) ->
     lists:foldr(fun alt/2, empty(), Ps).
 
 % Apply a parser N times.
--spec count(integer(), parser()) -> parser().
+-spec count(integer(), parser(A)) -> parser([A]).
 count(N, Parser) ->
     sequence(replicate(N, Parser)).
 
 % Run a parser between two others, returning the value parsed by the middle one.
--spec between(parser(), parser(), parser()) -> parser().
+-spec between(parser(A), parser(B), parser(C)) -> parser(B) when A :: term(), C :: term().
 between(Open, Between, Close) ->
     then(Open,
         bind(Between,
@@ -165,23 +168,23 @@ between(Open, Between, Close) ->
     ).
 
 % Applies a parser; if it fails, succeed with the given value.
--spec option(term(), parser()) -> parser().
+-spec option(A, parser(A)) -> parser(A).
 option(Default, Parser) ->
     alt(Parser, pure(Default)).
 
 % Apply a parser. Whether it succeeds or fails, succeed with 'ok'.
--spec optional(parser()) -> parser().
+-spec optional(parser(A)) -> parser(ok) when A :: term().
 optional(Parser) ->
     PureOk = pure(ok),
     alt(then(Parser, PureOk), PureOk).
 
 % Parse zero or more times, each separated by another parser.
--spec sep_by0(parser(), parser()) -> parser().
+-spec sep_by0(parser(A), parser(B)) -> parser([A]) when B :: term().
 sep_by0(Parser, Sep) ->
     alt(sep_by1(Parser, Sep), pure([])).
 
 % Parse one or more times, each separated by another parser.
--spec sep_by1(parser(), parser()) -> parser().
+-spec sep_by1(parser(A), parser(B)) -> parser([A]) when B :: term().
 sep_by1(Parser, Sep) ->
     bind(Parser,
         fun(X) ->
@@ -194,11 +197,11 @@ sep_by1(Parser, Sep) ->
         end
     ).
 
--spec end_by0(parser(), parser()) -> parser().
+-spec end_by0(parser(A), parser(B)) -> parser([A]) when B :: term().
 end_by0(Parser, Sep) ->
     many0(endby_body(Parser, Sep)).
 
--spec end_by1(parser(), parser()) -> parser().
+-spec end_by1(parser(A), parser(B)) -> parser([A]) when B :: term().
 end_by1(Parser, Sep) ->
     many1(endby_body(Parser, Sep)).
 
@@ -213,7 +216,7 @@ endby_body(Parser, Sep) ->
 % -----------------------------------------------------------------------------
 
 % Parse an unsigned, big-endian integer of the specified byte size.
--spec uint(integer(), string()) -> parser().
+-spec uint(integer(), string()) -> parser(non_neg_integer()).
 uint(Bytes, ErrReason) ->
     Bits = Bytes*8,
     fun
@@ -223,20 +226,20 @@ uint(Bytes, ErrReason) ->
             {error, {ErrReason, 0, Rest}}
     end.
 
--spec uint8() -> parser().
+-spec uint8() -> parser(non_neg_integer()).
 uint8() -> uint(1, "uint8").
 
--spec uint16() -> parser().
+-spec uint16() -> parser(non_neg_integer()).
 uint16() -> uint(2, "uint16").
 
--spec uint32() -> parser().
+-spec uint32() -> parser(non_neg_integer()).
 uint32() -> uint(4, "uint32").
 
--spec uint64() -> parser().
+-spec uint64() -> parser(non_neg_integer()).
 uint64() -> uint(8, "uint64").
 
 % Parse a binary of the specified length.
--spec binary(integer()) -> parser().
+-spec binary(non_neg_integer()) -> parser(binary()).
 binary(Bytes) ->
     fun
         (<<Binary:Bytes/binary, Rest/binary>>) ->
